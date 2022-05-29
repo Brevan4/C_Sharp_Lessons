bcd64bd95e6e9566eb53d8d60c12fad15167bf3d
CPK47 <101477195+CPK47@users.noreply.github.com>
2022-05-29 01:47:30 +0100

    Fix Enum

--- APD_Practical1/Months.cs
+++ APD_Practical1/Months.cs
@@ -7,98 +7,36 @@ using System.Threading.Tasks;
 namespace APD_Practical1
 {
     
-    public enum Month                                       //Enum of months
-        {
-            January, February, March, April, May, June, July, August, September, October, November, December
-        }
+    
     class Months
     {
+        public enum Month                                       //Enum of months
+        {
+            JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+        }
+
         public static void WhatMonth()                      //Method for user input
         {
 
-            Console.WriteLine("Please enter the first 3 letters of any month of the year (eg. Jan, Feb, Mar): ");
+            Console.WriteLine("Please enter the first 3 letters of any month of the year (eg. JAN, FEB, MAR...): ");
             string userInput = Console.ReadLine();
-            userInput = userInput.ToLower();
-                                    
-            switch (userInput)                              //Switch to select correct Enums and sentences
+            userInput = userInput.ToUpper();
+            Month month = Enum.Parse<Month>(userInput);
+            switch (month)                              //Switch to select correct Enums and sentences
             {
-                case "jan":
-                    {
-                        Console.WriteLine($"The first month of every year. It's {Month.January}.");
-                    }
-                    break;
-
-                case "feb":
-                    {
-                        Console.WriteLine($"The shortest month of the year. It's {Month.February}.");
-                    }
-                    break;
-
-                case "mar":
-                    {
-                        Console.WriteLine($"Heading forwards with a spring in your step. It's {Month.March}.");
-                    }
-                    break;
-
-                case "apr":
-                    {
-                        Console.WriteLine($"Fools get to play, but for only half a day. It's {Month.April}.");
-                    }
-                    break;
-
-                case "may":
-                    {
-                        Console.WriteLine($"Summer is right around the corner. It's {Month.May}.");
-                    }
-                    break;
-
-                case "jun":
-                    {
-                        Console.WriteLine($"Happy Birthday to me and the boss. It's {Month.June}.");
-                    }
-                    break;
-
-                case "jul":
-                    {
-                        Console.WriteLine($"Its marching season in my wee country. It's {Month.July}.");
-                    }
-                    break;
-
-                case "aug":
-                    {
-                        Console.WriteLine($"Enjoy the last few weeks of your time off kids, back to school soon. It's {Month.August}.");
-                    }
-                    break;
-
-                case "sep":
-                    {
-                        Console.WriteLine($"Do you remember? The 21st night of {Month.September}");
-                    }
-                    break;
-
-                case "oct":
-                    {
-                        Console.WriteLine($"Time for some Halloween Spooktacular fun in {Month.October}");
-                    }
-                    break;
-
-                case "nov":
-                    {
-                        Console.WriteLine($"Where I am from we burn bonfires in July, for normal people it's {Month.November}");
-                    }
-                    break;
-
-                case "dec":
-                    {
-                        Console.WriteLine($"Holidays are coming, holidays are coming... It's {Month.December}.");
-                    }
-                    break;
-
-                default:                                        //Default set as error for invalid user input
-                    {
-                        Console.WriteLine("Invalid month selected.");
-                    }
-                    break;
+                case Month.JAN:{Console.WriteLine("The first month of every year. It's January.");}break;
+                case Month.FEB:{Console.WriteLine("The shortest month of the year. It's February.");}break;
+                case Month.MAR:{Console.WriteLine("Heading forwards with a spring in your step. It's March.");}break;
+                case Month.APR:{Console.WriteLine("Fools get to play, but for only half a day. It's April.");}break;
+                case Month.MAY:{Console.WriteLine("Summer is right around the corner. It's May.");}break;
+                case Month.JUN:{Console.WriteLine("Happy Birthday to me and the boss. It's June.");}break;
+                case Month.JUL:{Console.WriteLine("Its marching season in my wee country. It's July.");}break;
+                case Month.AUG:{Console.WriteLine("Enjoy the last few weeks of your time off kids, back to school soon. It's August.");}break;
+                case Month.SEP:{Console.WriteLine("Do you remember? The 21st night of September.");}break;
+                case Month.OCT:{Console.WriteLine("Time for some Halloween Spooktacular fun in October");}break;
+                case Month.NOV:{Console.WriteLine("Where I am from we burn bonfires in July, for normal people it's November.");}break;
+                case Month.DEC:{Console.WriteLine("Holidays are coming, holidays are coming... It's December.");}break;
+                default:{Console.WriteLine("Invalid month selected.");}break;
             }
         }
     }
