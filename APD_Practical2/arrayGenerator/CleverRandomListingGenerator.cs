8bf1b45ff06d51762c2e0333826f0da55e0786d2
CPK47 <101477195+CPK47@users.noreply.github.com>
2022-06-06 12:01:49 +0100

    Update Test

--- APD_Practical2/arrayGenerator/CleverRandomListingGenerator.cs
+++ APD_Practical2/arrayGenerator/CleverRandomListingGenerator.cs
@@ -12,23 +12,13 @@ namespace APD_Practical2.arrayGenerator
 
         protected override void randomise()
         {
-            Array.Clear(array, 0, getArray().Length);
-            /*for (int i = 0; i < getArray().Length;)
-            {
-                int randomValue = getRandomIndex();
-                if (!array.Contains(randomValue))
-                { 
-                    array[i] = randomValue;
-                    i++;
-                }
-            } */
-            bool[] used = new bool[getSize()];                      //Set up of "used" array
+            int[] tempArray = getArray();
             for (int index = 0; index < getArray().Length;)
             {
                 int randomIndex = getRandomIndex();
                 if (used[randomIndex] == false)                     //if randomIndex of used array shows false, proceed
                 {
-                    array[index] = randomIndex;                      //Set index value to randomIndex value
+                    array[index] = randomIndex;                     //Set index value to randomIndex value
                     used[randomIndex] = true;                       //Set value of used randomIndex to True
                     index++;                                        //Increment index by 1
                 }
