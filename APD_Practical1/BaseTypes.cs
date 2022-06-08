1cb452eb475242887701bff0067a1ebed14e6923
Sania <u2057916@unimail.hud.ac.uk>
2022-06-08 19:58:58 +0100

    Update BaseTypes.cs

--- APD_Practical1/BaseTypes.cs
+++ APD_Practical1/BaseTypes.cs
@@ -9,53 +9,57 @@ namespace APD_Practical1
 
     class BaseTypes
     {
-        //1) Remove comments and assign correct datatypes bellow.
+        
+        static void Casting()
+        {
+            //1) Remove comments and assign correct datatypes bellow.
+            bool One = true;
+            bool Two = false;
+            char Three = 'Z';
+            char Four = '\\';
+            int Five = 100;
+            int Six = 10000;
+            int Seven = 1000000;
+            long Eight = 10000000000L;
+            float Nine = 3.14f;
+            double Ten = 2.71828d;
 
-        bool One = true;
-        bool Two = false;
-        char Three = 'Z';
-        char Four = '\\';
-        int Five = 100;
-        int Six = 10000;
-        int Seven = 1000000;
-        long Eight = 10000000000L;
-        float Nine = 3.14f;
-        double Ten = 2.71828d;
+            // 2) Attempted to cast each into types of boolean, char, integer, long, float.
+            //bool
+            //bool cast19 = Six;
+            //bool cast5 = Three;
+            //bool cast9 = Four;
+            //bool cast14 = Five;
 
+            //integer
+            //int cast1 = One;
+            int cast6 = Three;
+            int cast10 = Four;
+            int cast15 = Five;
+            int cast20 = Six;
 
-        // 2) Attempted to cast each into types of boolean, char, integer, long, float.
-        //one
-        int cast1 = One;
-        char cast2 = One;
-        long cast3 = One;
-        float cast4 = One;
+            //char
+            //char cast2 = One;
+            char cast13 = Four;
+            char cast18 = (char) Five;
+            char cast23 = (char) Six;
 
-        //three
-        bool cast5 = three;
-        int cast6 = three;
-        long cast7 = three;
-        float cast8 = three;
+            //long
+            //long cast3 = One;
+            long cast7 = Three;
+            long cast11 = Four;
+            long cast16 = Five;
+            long cast21 = Six;
 
-        //four
-        bool cast9 = four;
-        int cast10 = four;
-        long cast11 = four;
-        float cast12 = four;
-        char cast13 = four;
+            //float
+            //float cast4 = One;
+            float cast8 = Three;
+            float cast12 = Four;
+            float cast17 = Five;
+            float cast22 = Six;
 
-        //five
-        bool cast14 = five;
-        int cast15 = five;
-        long cast16 = five;
-        float cast17 = five;
-        char cast18 = five;
-
-        //eight
-        bool cast19 = five;
-        int cast20 = five;
-        long cast21 = five;
-        float cast22 = five;
-        char cast23 = five;
+        }
+        
 
         // Comment which type of casting is applicable Ie implicit, explicit Not possible  
 
@@ -63,8 +67,14 @@ namespace APD_Practical1
         // add function to print each casting out
         static void printCast()
         {
-            // Console.WriteLine(cast1);
+            //Console.WriteLine(cast1);
             //Console.WriteLine(cast2);
+            //look at generic methods
+
+            char[] charArray =
+            {
+
+            }; 
 
         }
     }
