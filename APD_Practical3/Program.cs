d18534cd2fec06dbb6adc37ecaa9b5fad2a8e8b7
Brevan4 <Brevan4@users.noreply.github.com>
2022-06-21 15:45:11 +0100

    Created New Project for  Practical 3
    DO NOT MAKE CHANGES TO PROJECT 1 OR 2

---
+++ APD_Practical3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APD_Practical3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
